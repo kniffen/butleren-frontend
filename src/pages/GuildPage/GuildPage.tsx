0873f3d77ab16f6b0fdb609623560830260ea5f8
k-lokoy <kniffen@knifftech.org>
2025-10-02 08:00:40 +0200

    Feature: Add loading card component

--- src/pages/GuildPage/GuildPage.tsx
+++ src/pages/GuildPage/GuildPage.tsx
@@ -6,12 +6,17 @@ import { GuildSettings } from '../../components/GuildSettings/GuildSettings';
 import { useParams } from 'react-router-dom';
 import { Modules } from '../../components/Modules/Modules';
 import { Commands } from '../../components/Commands/Commands';
+import { LoadingCard } from '../../components/LoadingCard/LoadingCard';
 
 export const GuildPage = function(): ReactNode {
   const { guild } = useAPI();
 
-  if (guild.isLoading) {
-    return <p>Loading...</p>;
+    if (guild.isLoading) {
+    return <>
+      <LoadingCard height="5rem" />
+      <LoadingCard />
+      <LoadingCard />
+    </>;
   }
 
   if (null === guild.data) {
