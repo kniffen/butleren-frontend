e7f5ef0b190058b7e996a8d401dbd823a4a78397
k-lokoy <kniffen@knifftech.org>
2025-10-05 18:56:57 +0200

    Feature: Add restore button for commands

--- src/components/Commands/Commands.tsx
+++ src/components/Commands/Commands.tsx
@@ -1,30 +1,15 @@
 import type { JSX } from 'react';
 import { useAPI } from '../../provider/hooks/useAPI';
-import { Card } from '../Card/Card';
-import { Toggle } from '../Toggle/Toggle';
+import { Command } from '../Command/Command';
 import './Commands.scss';
 
 export function Commands(): JSX.Element {
-  const { modules, commands } = useAPI();
+  const { commands } = useAPI();
 
   return (
     <div className="commands">
       {commands.data.map(command =>
-        <Card className="command-card" title={command.slug} key={command.slug}>
-          <Toggle
-            className="command-card__toggle"
-            defaultChecked={command.isEnabled || command.isLocked}
-            isLocked={command.isLocked}
-            onChange={async (checked: boolean) => {
-              await commands.updateSettings(command.slug, { isEnabled: checked });
-              await Promise.all([
-                commands.update(),
-                modules.updateModules()
-              ]);
-            }}
-          />
-          <p className="command-card__description">{command.description}</p>
-        </Card>
+        <Command command={command} key={command.slug} />
       )}
     </div>
   );
